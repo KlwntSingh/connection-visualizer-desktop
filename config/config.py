5ecf38cfbc4e7aa3d5c792c1b3b3800d15070596
kulwantDal <kulwant@dal.ca>
2018-02-22 02:16:42 -0400

    1. commented out inhouse api i.e geekasservice.com api 2. using new api from extreme-api lookup api

--- config/config.py
+++ config/config.py
@@ -8,4 +8,6 @@ CONFIG = json.load(open("config/config.json"))
 # CONFIG["AUTHOR"] =  """Kulwant Singh\nkulwantbughipura@gmail.com\nhttps://www.github.com/kulwantDal/sniffer"""
 CONFIG["AUTHOR"] = ["Kulwant Singh", "kulwantbughipura@gmail.com", "https://www.github.com/kulwantDal/monitoring"]
 
+#"IP_API" = "https://ip-geo.geekasservice.com/info/"
+
 CONFIG["os"] = platform.system()
