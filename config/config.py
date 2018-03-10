4ebf4b43a9891939008976afad3afd3000c38aaa
kulwantDal <kulwant@dal.ca>
2018-03-10 17:09:00 -0400

    github repo updated

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 CONFIG = json.load(open("config/config.json"))
 
 # CONFIG["AUTHOR"] =  """Kulwant Singh\nkulwantbughipura@gmail.com\nhttps://www.github.com/kulwantDal/sniffer"""
-CONFIG["AUTHOR"] = ["Kulwant Singh", "kulwantbughipura@gmail.com", "https://www.github.com/kulwantDal/monitoring"]
+CONFIG["AUTHOR"] = ["Kulwant Singh", "kulwantbughipura@gmail.com", "https://www.github.com/kulwantDal/connection-visualizer-desktop"]
 
 #"IP_API" = "https://ip-geo.geekasservice.com/info/"
 
